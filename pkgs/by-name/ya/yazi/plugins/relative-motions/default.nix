d8514e70b54a28a235ed3d0d4a268453da959cbc
César González Girón <ipog71@gmail.com>
2026-09-18 13:26:07 -0600

    Change GitHub owner for relative-motions plugin

--- pkgs/by-name/ya/yazi/plugins/relative-motions/default.nix
+++ pkgs/by-name/ya/yazi/plugins/relative-motions/default.nix
@@ -8,10 +8,9 @@ mkYaziPlugin {
   version = "0-unstable-2025-07-09";
 
   src = fetchFromGitHub {
-    owner = "dedukun";
+    owner = "Jormala";
     repo = "relative-motions.yazi";
-    rev = "a603d9ea924dfc0610bcf9d3129e7cba605d4501";
-    hash = "sha256-9i6x/VxGOA3bB3FPieB7mQ1zGaMK5wnMhYqsq4CvaM4=";
+    hash = "sha256-lyzwbs1u4qXuIamE31QAD6e22RPJxROs7Q/tuTkz12Q=";
   };
 
   meta = {
